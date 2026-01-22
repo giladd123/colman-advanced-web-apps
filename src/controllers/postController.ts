7048e0118533570fde8858b19fd9e617039447e7
Gilad Tidhar <giladtidhar19@gmail.com>
2026-01-22 12:15:55 +0200

    fix graceful shutdown, add updatePost route

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -4,3 +4,16 @@ export async function addPost(title: string, sender: string, content: string) {
   const newPost = new postModel({ title, sender, content });
   return await newPost.save();
 }
+
+export async function updatePost(
+  postId: string,
+  title?: string,
+  sender?: string,
+  content?: string,
+) {
+  const updateData: any = {};
+  if (title) updateData.title = title;
+  if (sender) updateData.sender = sender;
+  if (content) updateData.content = content;
+  return await postModel.findByIdAndUpdate(postId, updateData, { new: true });
+}
--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -1,6 +1,9 @@
 import { Request, Router } from "express";
-import { addPost } from "../controllers/postController";
-import { addPostValidator } from "../middleware/postValidator";
+import { addPost, updatePost } from "../controllers/postController";
+import {
+  addPostValidator,
+  putPostValidator,
+} from "../middleware/postValidator";
 
 export const postRouter = Router();
 
@@ -11,5 +14,20 @@ postRouter.post("/", addPostValidator, async (req: Request, res) => {
     return res.status(201).json({ message: "Post added successfully" });
   } catch (error) {
     return res.status(500).json({ error: "Failed to add post" });
+  }
+});
+
+postRouter.put("/:postId", putPostValidator, async (req: Request, res) => {
+  const postId = req.params.postId as string; // we know that postId is a string from the validator
+
+  const { title, sender, content } = req.body;
+  try {
+    const updatedPost = await updatePost(postId, title, sender, content);
+    if (!updatedPost) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    return res.status(200).json({ message: "Post updated successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to update post" });
   }
 });
