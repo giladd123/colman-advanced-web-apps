59a4343440a3ade7e3b563ad3bfb4c4ab789ca1a
Gilad Tidhar <giladtidhar19@gmail.com>
2026-02-24 22:16:28 +0200

    add port to ecosystem config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -7,10 +7,12 @@ module.exports = {
 
       env: {
         NODE_ENV: "development",
+        PORT: 3000,
       },
 
       env_production: {
         NODE_ENV: "production",
+        PORT: 443,
       },
     },
   ],
