b4677cd93053d9f8b1e2d32c256396a19e1d0881
Gilad Tidhar <giladtidhar19@gmail.com>
2026-02-24 22:21:58 +0200

    fix ssl paths

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import type { Express } from "express";
 import https from "https";
 import http from "http";
 import fs from "fs";
+import path from "path";
 
 let server: Server;
 
@@ -39,8 +40,8 @@ initApp()
         } else {
             console.log('Running in PRODUCTION mode with HTTPS');
             const sslOptions = {
-                key: fs.readFileSync('../ssl/client-key.pem'),
-                cert: fs.readFileSync('../ssl/client-cert.pem')
+                key: fs.readFileSync(path.resolve(__dirname, '../../ssl/client-key.pem')),
+                cert: fs.readFileSync(path.resolve(__dirname, '../../ssl/client-cert.pem'))
             };
            
             // const localHttpsPort = 3443; // Override port for local HTTPS testing
